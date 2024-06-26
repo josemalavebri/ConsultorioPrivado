fd09ab10f44bbb26512d217d755a9cb84d67f2a4
JEAN MARCO ROMERO SERRANO <jean.romeros@ug.edu.ec>
2024-06-26 07:45:00 -0500

    no salio completo :')

--- Vista/Paciente/Paciente_form.Designer.cs
+++ Vista/Paciente/Paciente_form.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paciente_form));
+            resetear_button = new Button();
+            nuevo_button = new Button();
+            buscar_button = new Button();
+            cedula_text = new TextBox();
+            label4 = new Label();
+            label1 = new Label();
+            medicos_dgv = new DataGridView();
+            Editar = new DataGridViewImageColumn();
+            Eliminar = new DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)medicos_dgv).BeginInit();
+            SuspendLayout();
+            // 
+            // resetear_button
+            // 
+            resetear_button.Location = new Point(353, 17);
+            resetear_button.Margin = new Padding(3, 4, 3, 4);
+            resetear_button.Name = "resetear_button";
+            resetear_button.Size = new Size(106, 41);
+            resetear_button.TabIndex = 29;
+            resetear_button.Text = "Resetear";
+            resetear_button.UseVisualStyleBackColor = true;
+            // 
+            // nuevo_button
+            // 
+            nuevo_button.Location = new Point(2, 75);
+            nuevo_button.Margin = new Padding(3, 4, 3, 4);
+            nuevo_button.Name = "nuevo_button";
+            nuevo_button.Size = new Size(197, 63);
+            nuevo_button.TabIndex = 28;
+            nuevo_button.Text = "Nuevo";
+            nuevo_button.UseVisualStyleBackColor = true;
+            // 
+            // buscar_button
+            // 
+            buscar_button.Location = new Point(222, 19);
+            buscar_button.Margin = new Padding(3, 4, 3, 4);
+            buscar_button.Name = "buscar_button";
+            buscar_button.Size = new Size(110, 39);
+            buscar_button.TabIndex = 27;
+            buscar_button.Text = "Buscar";
+            buscar_button.UseVisualStyleBackColor = true;
+            // 
+            // cedula_text
+            // 
+            cedula_text.Location = new Point(73, 25);
+            cedula_text.Margin = new Padding(3, 4, 3, 4);
+            cedula_text.Name = "cedula_text";
+            cedula_text.Size = new Size(114, 27);
+            cedula_text.TabIndex = 26;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 20);
+            label4.TabIndex = 25;
+            label4.Text = "Cedula";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(2, 157);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 20);
+            label1.TabIndex = 24;
+            label1.Text = "Medicos Agregados:";
+            // 
+            // medicos_dgv
+            // 
+            medicos_dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            medicos_dgv.Columns.AddRange(new DataGridViewColumn[] { Editar, Eliminar });
+            medicos_dgv.Location = new Point(5, 201);
+            medicos_dgv.Margin = new Padding(3, 4, 3, 4);
+            medicos_dgv.Name = "medicos_dgv";
+            medicos_dgv.RowHeadersWidth = 51;
+            medicos_dgv.Size = new Size(973, 255);
+            medicos_dgv.TabIndex = 23;
+            // 
+            // Editar
+            // 
+            Editar.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Editar.HeaderText = "Editar";
+            Editar.Image = Properties.Resources._8188360;
+            Editar.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            Editar.MinimumWidth = 6;
+            Editar.Name = "Editar";
+            Editar.Width = 54;
+            // 
+            // Eliminar
+            // 
+            Eliminar.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Eliminar.HeaderText = "Eliminar";
+            Eliminar.Image = (Image)resources.GetObject("Eliminar.Image");
+            Eliminar.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            Eliminar.MinimumWidth = 6;
+            Eliminar.Name = "Eliminar";
+            Eliminar.Width = 69;
+            // 
+            // Paciente_form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 534);
+            Controls.Add(resetear_button);
+            Controls.Add(nuevo_button);
+            Controls.Add(buscar_button);
+            Controls.Add(cedula_text);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(medicos_dgv);
+            Name = "Paciente_form";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)medicos_dgv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button resetear_button;
+        private Button nuevo_button;
+        private Button buscar_button;
+        private TextBox cedula_text;
+        private Label label4;
+        private Label label1;
+        private DataGridView medicos_dgv;
+        private DataGridViewImageColumn Editar;
+        private DataGridViewImageColumn Eliminar;
     }
 }
--- Vista/Paciente/Pacienteadd_form.Designer.cs
+++ Vista/Paciente/Pacienteadd_form.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Pacienteadd_form";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(184, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(184, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(184, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Cedula";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(184, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(184, 98);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Nombre";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(184, 183);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(184, 160);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Apellido";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(184, 246);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(184, 289);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Telefono";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(184, 312);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(184, 223);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Correo";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(92, 385);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 11;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(319, 385);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 12;
+            button2.Text = "Regresar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Pacienteadd_form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(516, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Pacienteadd_form";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
     }
 }
